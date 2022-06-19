49b7e98eb971e2f5ce235ff5f6f55983ba5b0c1b
Stephen Leander <stephen.leander1@gmail.com>
2022-06-19 11:29:06 +0200

    Setup Tailwind configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="text-gray-500">Hello World</div>
+    </div>
+  );
 };
 
 export default App;
