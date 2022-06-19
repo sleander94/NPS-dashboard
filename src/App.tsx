2a0bcbea5ea534d372fb55dcb50ac393f9ef026a
Stephen Leander <stephen.leander1@gmail.com>
2022-06-19 14:47:10 +0200

    Fetch list of all parks & basic info on page load

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const App = () => {
+  const [parksInfo, setParksInfo] = useState<any[]>([]);
+
+  const NPSKey = 'GutNTqgBFaepYpX1aGjggwDBjLiKJk8PMDCUnXsf';
+
+  useEffect(() => {
+    const getParksInfo = async () => {
+      try {
+        const response = await fetch(
+          `https://developer.nps.gov/api/v1/parks?limit=999&api_key=${NPSKey}`
+        );
+        const data = await response.json();
+        setParksInfo(data.data);
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    getParksInfo();
+  }, []);
+
   return (
     <div className="App">
-      <div className="text-gray-500">Hello World</div>
+      {/* {parksInfo.length > 0 && (
+        <div className="p-4 text-xl">
+          {parksInfo.map((park) => {
+            return (
+              <div key={parksInfo.indexOf(park)}>
+                {parksInfo.indexOf(park)}. Name: {park.name} Code:{' '}
+                {park.parkCode}
+              </div>
+            );
+          })} 
+        </div> 
+      )} */}
     </div>
   );
 };
