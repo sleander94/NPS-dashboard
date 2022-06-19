9e4e1b8de85c4b20a976a0210b5d83071f2555db
Stephen Leander <stephen.leander1@gmail.com>
2022-06-19 19:44:42 +0200

    Get parkCode from useParams, style alert

--- src/components/Alerts.tsx
+++ src/components/Alerts.tsx
@@ -1,8 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
-type AlertsProps = { parkCode: string };
+type AlertsParams = { parkCode: string };
 
-const Alerts = ({ parkCode }: AlertsProps) => {
+const Alerts = () => {
+  let { parkCode } = useParams<AlertsParams>();
   const [alerts, setAlerts] = useState<any[]>([]);
 
   useEffect(() => {
@@ -13,7 +15,6 @@ const Alerts = ({ parkCode }: AlertsProps) => {
         );
         const data = await response.json();
         setAlerts(data.data);
-        console.log('getting alerts');
       } catch (e) {
         console.error(e);
       }
@@ -21,13 +22,15 @@ const Alerts = ({ parkCode }: AlertsProps) => {
     getAlerts();
   }, []);
   return (
-    <div>
+    <div className="w-1/3 h-3/6 p-2 border border-black overflow-y-scroll">
+      <h1 className="text-center text-2xl font-bold">Alerts</h1>
       {alerts.length > 0 &&
         alerts.map((alert) => {
           return (
-            <div key={alerts.indexOf(alert)}>
-              <div>{alert.title}</div>
-              <div>{alert.description}</div>
+            <div className="p-1" key={alerts.indexOf(alert)}>
+              <p className="text-lg font-semibold">{alert.title}</p>
+              <p>{alert.lastIndexedDate}</p>
+              <p>{alert.description}</p>
             </div>
           );
         })}
