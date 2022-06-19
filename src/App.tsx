1db697f768b9e3b94cfec7163a7ec8d13d516314
Stephen Leander <stephen.leander1@gmail.com>
2022-06-19 15:58:09 +0200

    Add search bar to filter parksInfo and display relevant parks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import ParkPage from './components/ParkPage';
 
 const App = () => {
   const [parksInfo, setParksInfo] = useState<any[]>([]);
+  const [searchVal, setSearchVal] = useState('');
 
   const NPSKey = 'GutNTqgBFaepYpX1aGjggwDBjLiKJk8PMDCUnXsf';
 
@@ -12,6 +14,7 @@ const App = () => {
           `https://developer.nps.gov/api/v1/parks?limit=999&api_key=${NPSKey}`
         );
         const data = await response.json();
+        console.log(data);
         setParksInfo(data.data);
       } catch (e) {
         console.error(e);
@@ -22,18 +25,29 @@ const App = () => {
 
   return (
     <div className="App">
-      {/* {parksInfo.length > 0 && (
-        <div className="p-4 text-xl">
-          {parksInfo.map((park) => {
-            return (
-              <div key={parksInfo.indexOf(park)}>
-                {parksInfo.indexOf(park)}. Name: {park.name} Code:{' '}
-                {park.parkCode}
-              </div>
-            );
-          })} 
-        </div> 
-      )} */}
+      <input
+        type="text"
+        className="border border-black m-4"
+        onChange={(e) => setSearchVal(e.target.value)}
+      ></input>
+      <button className="border border-black m-4">Search</button>
+      {/*   <ParkPage parkCode="arch"></ParkPage> */}
+
+      {parksInfo
+        .filter((park) => {
+          if (searchVal === '') {
+            return park;
+          } else if (
+            park.name
+              .toLocaleLowerCase()
+              .includes(searchVal.toLocaleLowerCase())
+          ) {
+            return park;
+          }
+        })
+        .map((park) => {
+          return <div key={parksInfo.indexOf(park)}>{park.name}</div>;
+        })}
     </div>
   );
 };
