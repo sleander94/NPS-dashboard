420dd5a431320550743b30350f38f8d04c693261
Stephen Leander <stephen.leander1@gmail.com>
2022-06-19 17:06:45 +0200

    Move searchbar to new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import ParkPage from './components/ParkPage';
+import Searchbar from './components/Searchbar';
 
 const App = () => {
   const [parksInfo, setParksInfo] = useState<any[]>([]);
-  const [searchVal, setSearchVal] = useState('');
 
   const NPSKey = 'GutNTqgBFaepYpX1aGjggwDBjLiKJk8PMDCUnXsf';
 
@@ -14,7 +13,6 @@ const App = () => {
           `https://developer.nps.gov/api/v1/parks?limit=999&api_key=${NPSKey}`
         );
         const data = await response.json();
-        console.log(data);
         setParksInfo(data.data);
       } catch (e) {
         console.error(e);
@@ -25,29 +23,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <input
-        type="text"
-        className="border border-black m-4"
-        onChange={(e) => setSearchVal(e.target.value)}
-      ></input>
-      <button className="border border-black m-4">Search</button>
-      {/*   <ParkPage parkCode="arch"></ParkPage> */}
-
-      {parksInfo
-        .filter((park) => {
-          if (searchVal === '') {
-            return park;
-          } else if (
-            park.name
-              .toLocaleLowerCase()
-              .includes(searchVal.toLocaleLowerCase())
-          ) {
-            return park;
-          }
-        })
-        .map((park) => {
-          return <div key={parksInfo.indexOf(park)}>{park.name}</div>;
-        })}
+      <Searchbar parksInfo={parksInfo}></Searchbar>
     </div>
   );
 };
