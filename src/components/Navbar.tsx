a8448cad1300c4531bfb32a8f81305b6bea6845b
Stephen Leander <stephen.leander1@gmail.com>
2022-06-20 15:47:16 +0200

    Change output from div to nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,12 +3,12 @@ import logo from '../images/mountain-svgrepo-com.svg';
 
 const Navbar = () => {
   return (
-    <div className="p-3 h-[72px] flex justify-center items-center">
+    <nav className="p-3 h-[72px] flex justify-center items-center">
       <Link className="flex justify-center items-center gap-2" to="/home">
         <img className="w-14 h-14" src={logo} alt=""></img>
         <h1 className="text-xl font-bold">National Parks Dashboard</h1>
       </Link>
-    </div>
+    </nav>
   );
 };
 
