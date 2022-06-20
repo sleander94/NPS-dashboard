f70aa309c03c735e20f5c8c01a55f9c54f5ebd0d
Stephen Leander <stephen.leander1@gmail.com>
2022-06-20 11:07:37 +0200

    Initial Navbar commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+import logo from '../images/mountain-svgrepo-com.svg';
+
+const Navbar = () => {
+  return (
+    <div className="p-3 h-[72px] flex justify-center items-center">
+      <Link className="flex justify-center items-center gap-2" to="/home">
+        <img className="w-14 h-14" src={logo} alt=""></img>
+        <h1 className="text-xl font-bold">National Parks Dashboard</h1>
+      </Link>
+    </div>
+  );
+};
+
+export default Navbar;
