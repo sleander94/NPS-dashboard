3bc40146a44932ad24d09094f00b21f76ebbc0e6
Stephen Leander <stephen.leander1@gmail.com>
2022-06-20 11:09:21 +0200

    Initial HomePage commit

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,29 @@
+import Searchbar from './Searchbar';
+
+type HomePageParams = {
+  parksInfo: Array<any>;
+};
+
+const HomePage = ({ parksInfo }: HomePageParams) => {
+  const hideSearch = () => {
+    const suggs = document.getElementById('searchSuggs');
+    if (suggs !== null) {
+      suggs.classList.add('hidden');
+      suggs.classList.remove('visible');
+    }
+  };
+
+  return (
+    <div className="h-[calc(100vh-72px)] grid grid-auto-rows items-start">
+      <div
+        className="h-full flex justify-center bg-arches bg-center bg-no-repeat row-start-1 row-end-1 col-start-1 col-end-1"
+        onClick={() => hideSearch()}
+      ></div>
+      <div className="mt-12 sm:mt-48 w-3/5 max-w-xl min-w-[300px] row-start-1 row-end-1 col-start-1 col-end-1 justify-self-center">
+        <Searchbar parksInfo={parksInfo} />
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
