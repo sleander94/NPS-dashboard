3ad71dd13bbcfe945f2d9bf574d265ee16b5622f
Stephen Leander <stephen.leander1@gmail.com>
2022-06-22 10:15:16 +0200

    Edit styles, move park name to ParkNav

--- src/components/GeneralInformation.tsx
+++ src/components/GeneralInformation.tsx
@@ -1,6 +1,3 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-
 type parkProps = {
   park: {
     fullName?: string;
@@ -17,15 +14,33 @@ type parkProps = {
 
 const GeneralInformation = ({ park }: parkProps) => {
   return (
-    <div className="GeneralInfo w-full  max-w-[520px] h-3/8 max-h-[375px] border border-black rounded overflow-y-scroll">
-      <h1 className="text-center text-2xl font-bold bg-lime-600 text-white">
-        General Information
-      </h1>
-      {park.images && <img src={park.images[0].url}></img>}
-      <p>{park.description}</p>
-      <p>{park.weatherInfo}</p>
-      <a href={park.url}>NPS Website</a>
-      <a href={park.directionsUrl}>Directions</a>
+    <div className="GeneralInfo w-full  max-w-[520px] border border-black rounded bg-white">
+      {park.images && (
+        <img
+          className="rounded-tr-[.19rem] rounded-tl-[.19rem]"
+          src={park.images[0].url}
+          alt={park.images[0].altText}
+        ></img>
+      )}
+      <p className="p-1">{park.description}</p>
+      <div className="p-2 pb-4 flex justify-around">
+        <a
+          className="rounded pl-1 pr-1 border-2 border-[#97c64b] font-semibold"
+          href={park.url}
+          target="_blank"
+        >
+          NPS Website
+        </a>
+        <a
+          className="rounded pl-1 pr-1 border-2 border-[#97c64b] font-semibold"
+          href={park.directionsUrl}
+          target="_blank"
+        >
+          Directions
+        </a>
+      </div>
+      <h2 className="text-center font-bold">Typical Conditions</h2>
+      <p className="p-2">{park.weatherInfo}</p>
     </div>
   );
 };
