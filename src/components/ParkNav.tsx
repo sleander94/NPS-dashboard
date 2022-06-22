1ab4328d8e830f5fa03ab89f3bc92b7b7dd7ae10
Stephen Leander <stephen.leander1@gmail.com>
2022-06-22 10:17:31 +0200

    Edit styles, remove unused imports

--- src/components/ParkNav.tsx
+++ src/components/ParkNav.tsx
@@ -17,11 +17,14 @@ type parkProps = {
 
 const ParkNav = ({ park }: parkProps) => {
   return (
-    <div className="ParkNav p-1 w-full">
-      <a href="#info" className="text-xl font-bold">
+    <div className="ParkNav mt-3 w-full h-[84px] text-center sticky top-0 bg-white border-b border-black">
+      <a
+        href="#info"
+        className=" block text-xl p-2 font-bold bg-[#97c64b] border-b border-t border-black"
+      >
         {park.fullName}
       </a>
-      <div className="LinkContainer">
+      <div className="LinkContainer w-full p-2 pb-0 flex justify-between items-center bg-white">
         <a href="#weather">Weather</a>
         <a href="#alerts">Alerts</a>
         <a href="#news">News</a>
