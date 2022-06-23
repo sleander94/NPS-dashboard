2851039e3bac387f603df404d1a29ee0a91dc9e7
Stephen Leander <stephen.leander1@gmail.com>
2022-06-23 16:17:49 +0200

    Edit h1 text

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <nav id="info" className="p-3 h-[72px] flex justify-center items-center">
       <Link className="flex justify-center items-center gap-2" to="/home">
         <img className="w-14 h-14" src={logo} alt=""></img>
-        <h1 className="text-xl font-bold">National Parks Dashboard</h1>
+        <h1 className="text-xl font-bold">Parks At A Glance</h1>
       </Link>
     </nav>
   );
