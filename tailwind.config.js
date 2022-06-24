a80559c280c05ccb1bf4f9e88d65033d4c603dc2
Stephen Leander <stephen.leander1@gmail.com>
2022-06-24 11:19:29 +0200

    Extend custom background image & media query sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'smoky-mountains': "url('/src/images/smoky-mountains.jpg')",
+      },
+    },
+    screens: {
+      xs: '405px',
+      sm: '640px',
+      lg: '1080px',
+      xl: '1205px',
+      xxl: '1700px',
+    },
   },
   plugins: [],
 };
