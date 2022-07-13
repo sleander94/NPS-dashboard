5ed01efd0eb3aa8e19c39c8fdee2765c2e7b8d2c
Stephen Leander <stephen.leander1@gmail.com>
2022-07-13 17:17:35 -0600

    Add overflow-y scroll

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -72,7 +72,7 @@ const Weather = ({ park }: ParkProp) => {
   return (
     <div
       id="weather"
-      className="Weather overflow-hidden grid justify-items-center w-full lg:max-w-[640px] min-w-[320px] max-h-[400px] lg:min-h-[50%] border border-black rounded bg-white"
+      className="Weather overflow-scroll grid justify-items-center w-full lg:max-w-[640px] min-w-[320px] max-h-[400px] lg:min-h-[50%] border border-black rounded bg-white"
     >
       <h1 className="WeatherTitle justify-self-stretch h-[33px] text-center text-2xl font-bold bg-[#3A736C] text-white rounded-tr-[.19rem] rounded-tl-[.19rem] border-b border-black">
         Current Weather
