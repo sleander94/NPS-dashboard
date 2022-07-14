944cc9c65cf65724a72127afe73dac36bfbd5f4e
Stephen Leander <stephen.leander1@gmail.com>
2022-07-14 10:05:23 -0600

    Add active class to link for current section

--- src/components/ParkNav.tsx
+++ src/components/ParkNav.tsx
@@ -2,6 +2,26 @@ import { ParkProp } from './ParkPage';
 import { hideSearch } from './Searchbar';
 
 const ParkNav = ({ park }: ParkProp) => {
+  let sections = document.querySelectorAll('section');
+  let menu = document.querySelectorAll('.ParkNav a');
+  window.onscroll = () => {
+    sections.forEach((section) => {
+      let top = window.scrollY;
+      let offset = section.offsetTop - 91;
+      let height = section.offsetHeight;
+      let id = section.getAttribute('id');
+      if (top >= offset && top < offset + height) {
+        menu.forEach((link) => {
+         link.classList.remove('active');
+         if(!document.querySelector('.ParkNav a[href*=' + id + ']')!.classList.contains('info')) { 
+          document
+            .querySelector('.ParkNav a[href*=' + id + ']')!
+            .classList.add('active');
+         }
+        });
+      }
+    });
+  };
   return (
     <div
       className="ParkNav z-10 mt-3 w-full h-[84px] lg:h-[46px] text-center sticky top-0 bg-white border-b sm:border-b-0 border-black"
@@ -9,7 +29,7 @@ const ParkNav = ({ park }: ParkProp) => {
     >
       <a
         href="#info"
-        className=" block text-xl p-2 font-bold bg-[#3A736C] text-white border-b border-t border-black"
+        className="info block text-xl p-2 font-bold bg-[#3A736C] text-white border-b border-t border-black"
       >
         {park.fullName}
       </a>
